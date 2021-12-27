5a3320e2e723b64fe95085141f5f55144437a6d8
Nadine <nadine.spychala@gmail.com>
2021-12-27 20:59:50 +0000

    fixed broken parameter sweep, included micro and macro variables as parameters

---
+++ data/statdata_coup_errors1.m
@@ -0,0 +1,87 @@
+function X = statdata_coup_errors1(coupling_matrix, npoints, tau, err)
+
+% -----------------------------------------------------------------------
+%   FUNCTION: statdata_coup_errors1.m
+%   PURPOSE:  Obtain time-series data for a Gaussian MVAR(p) process
+%             X_t=A_1*X_{t-1}+A_2*X_{t-2}+...+A_p*X_{t-p}+E_t
+%
+%   INPUT:  coupling_matrix - generalized connectivity matrix, A=(A_1 A_2 ... A_p)
+%		    npoints - number of time-steps
+%		    tau - time-lag
+%               err - covariance matrix for E_t
+%
+%   OUTPUT: X - time-series data, rows are variables, columns are observations
+% -----------------------------------------------------------------------
+
+settle=500; %will only keep post-equilibrium data points
+npoints = npoints+settle;
+nvar=size(coupling_matrix,1); 
+
+%% simulate time-series of correlated error terms
+
+% simulate time-series of correlated errors (by multiplying M with the upper triangular matrix L obtained from the Cholesky decomposition of the desired correlation matrix R) 
+% but: I get different values for corr, which might be due to the fact that chol might fail, if the covarince matrix is singular or near singular. 
+%{
+mu = 0;
+sigma = 1;
+M = mu + sigma*randn(N,size(coupling_matrix,2));
+R = [1 c; c 1];
+L = chol(R);
+E = (M*L)';
+
+corr_errors = corrcoef(E(1,:),E(2,:));
+%} 
+
+% alternative: simulate errors from correlated multivariate standard normal distribution
+% but: this can yield different actual correlations for them; the chosen correlation will only be yielded, if sample size is large 
+mu = zeros(1, nvar);
+
+% construct correlation matrix
+R = eye(nvar);
+R(R==0)=err;
+
+% only necessary for non-standard normal distributions, otherwise R = cov_matrix
+standard_dev = ones(1, nvar);							   % vector of standard deviations of the errors
+cov_err = diag(standard_dev)*R*diag(standard_dev);	 % diag() converts std vector to matrix where the diagonal entries are the stds, and all other entries 0
+%rng(1);
+E = mvnrnd(mu,cov_err,npoints)';						% simulate correlated errors
+
+corr_errors = corrcoef(E(1,:), E(2,:));					   % check correlation
+
+%% simulate time-series of the network
+
+p=floor(size(coupling_matrix,2)/nvar);								  % time-steps to look into the past
+
+X = zeros([nvar, npoints]);
+
+% non-vectorized version
+%{ 
+for i=tau+1:npoints
+    for j=1:nvar
+        for k=1:nvar
+            for m=1:tau
+			X(j,i)= (A(j,k+(m-1)*nvar)*X(k,i-m)+E(j,i));
+            end
+        end
+    end
+end 
+%} 
+
+% vectorized version
+for t=(1+tau):npoints
+	X(:,t) = coupling_matrix*X(:,t-tau) + E(:,t);
+end
+	
+% alternative: drawing *at each time-step* error values from a correlated 
+% multi-dimensional Gaussian, and adding it to the A*X_(t-1) term
+%{
+rng(1);
+for t=tau+1:npoints
+	X(:,t) = A*X(:,t-tau) + mvnrnd(zeros([1, size(A,1)]), cov_err)';
+end
+%}
+	
+X = X(:,settle+1:end);
+
+end
+	
--- scripts/emergence_complexity_measures_analysis.py
+++ scripts/emergence_complexity_measures_analysis.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
+'''
 Created on Wed Aug  4 13:55:56 2021
 
 @author: nadinespy
@@ -8,183 +8,156 @@ Created on Wed Aug  4 13:55:56 2021
 This script calculates emergence capacity using ecmc.compute_emergence() and saves
 everything in a pandas dataframe.
 
-""" 
+''' 
 
-import os
-from oct2py import Oct2Py
-import numpy as np
-import os.path as op
-from importlib import reload 
-import pandas as pd
-import pickle
-import random
-from itertools import product
-from functools import partial
-
-oc = Oct2Py()
+# TODO: make library installable
 # TODO: make lines only ~80 long
     
+import os
+from oct2py import octave as oc 
+import numpy as np
+import pandas as pd
  
 #%% 
-if __name__ == "__main__":
-    # TODO: make library installable
+
+if __name__ == '__main__':
+    
+
     # paths
     os.chdir('/media/nadinespy/NewVolume/my_stuff/work/PhD/my_projects/EmergenceComplexityMeasuresComparison/EmergenceComplexityMeasuresComparison_Python')
     import emergence_complexity_measures_comparison as ecmc 
-
     
-    analyses_pathout = '//media/nadinespy/NewVolume/my_stuff/work/PhD/my_projects/EmergenceComplexityMeasuresComparison/EmergenceComplexityMeasuresComparison_Python/results/analyses/'
-    plots_pathout = '//media/nadinespy/NewVolume/my_stuff/work/PhD/my_projects/EmergenceComplexityMeasuresComparison/EmergenceComplexityMeasuresComparison_Python/results/plots/'
-    data_pathin = op.join(ecmc.__path__[0], 'data')
+    os.chdir('/media/nadinespy/NewVolume/my_stuff/work/PhD/my_projects/EmergenceComplexityMeasuresComparison/EmergenceComplexityMeasuresComparison_Python/data')
+    import data_simulation as ds
+
+    main_directory = '//media/nadinespy/NewVolume/my_stuff/work/PhD/my_projects/EmergenceComplexityMeasuresComparison/EmergenceComplexityMeasuresComparison_Python'
+    analyses_pathout = main_directory + '/results/analyses/'
+    plots_pathout = main_directory + '/results/plots/'
+    data_pathin = main_directory + '/data/'
 
 # -----------------------------------------------------------------------------
-# causal emergence (based on phiid and practical)
+# MODEL & MEASURE PARAMETERS
 # -----------------------------------------------------------------------------
 
-#% load/generate data
+# creating pandas dataframe with the following variables as columns:
 
-# CREATING PANDAS DATAFRAME with the following columns:
-
-# model:            'two_node_mvar_model'
-#                   'eight_node_mvar_model_different_architectures'
-#                   'eight_node_mvar_model_global_coupling'
-#                   'eight_node_mvar_model_erdoes_renyi'
+# VARIABLES SPECIFIED BY USER:
+    
+# model:            '2node_mvar'
 
 # noise_corr:       100 values (noise correlations) going from 0.01 to 0.9 with equal steps between them
-# coupling:         for 'two_node_mvar_model': 100 values (coupling strengths) going from 0.045 to 0.45 with equal steps between them 
-#                   for 'eight_node_mvar_model_different_architectures': 7 distinctly different connectivity matrices:  'optimal_A' 
-#                                                                                                                       'optimal_B'
-#                                                                                                                       'small_world'
-#                                                                                                                       'two_communities'
-#                                                                                                                       'fully_connected'
-#                                                                                                                       'ring'
-#                                                                                                                       'uni_ring'
-#                   for 'eight_node_mvar_model_global_coupling' & 
-#                   'eight_node_mvar_model_erdoes_renyi': 100 values (being either global coupling factors or densities) going from 0.05 to 0.9 with equal steps between them
+# coupling:         for '2node_mvar': 100 values (coupling strengths) going from 0.045 to 0.45 with equal steps between them 
+#                   for '8node_mvar_diff_top': 7 distinctly different connectivity matrices:  'optimal_A' 
+#                                                                                             'optimal_B'
+#                                                                                             'small_world'
+#                                                                                             'two_communities'
+#                                                                                             'fully_connected'
+#                                                                                             'ring'
+#                                                                                             'uni_ring'
+#                   for '8node_mvar_global_coup' & 
+#                   '8node_mvar_er': 100 values (being either global coupling factors or densities) going from 0.05 to 0.9 with equal steps between them
 
-# time_lag:         2 values (time lags)
+# macro:        time-series (of length npoints) of macro variable: 
+#                   sum_micro_mvar
 
-# measure:          'phiid_emergence_capacity'
-#                   'phiid_downward_causation'
-#                   'phiid_causal_decoupling'
-#                   'practical_emergence_capacity'
-#                   'practical_downward_causation'
-#                   'practical_causal_decoupling'
+# micro:        time-series (of length npoints) of micro variables:   
+#                   raw_micro_mvar   
+
+# time_lag:         1, 10, 100 
+
+
+# VARIABLES SPECIFIED BY ECMC LIBRARY:
+# measure:          'causal_emergence_phiid' (dict with causal_emergence_phiid, downward_causation_phiid, causal_decoupling_phiid)
+#                   'causal_emergence_practical' (dict with causal_emergence_pract, downward_causation_pract, causal_decoupling_pract)
 #                   'dynamical_independence'
 #                   'g_emergence'
 
 # redundancy_func:  'mmi'
 #                   'ccs'      
 
-# macro_var:        time-series (of length npoints) of macro variable    
-
 # value:            value of emergence measure
 
-emergence_df_temp = []
-
-time_lags = [1, 3] 
-npoints = 2000
-#emergence_measures = ["causal_emergence_phiid", "causal_emergence_practical"] #, "dynamical_independence", "g_emergence"]
 
 
-def get_results_for_measure(measure_func, measure_params, data, measure_name):
-    emergence_df_temp = []
-    for params in product(*[params for pname, params in measure_param_dict.items()]):
-        temp_param_dict = {param_name: param for param_name, param in zip(measure_param_dict, params)} # just add variable names to param combination
-
-        emergence_result = measure_func(data, **temp_param_dict)
-        # TODO: refactor so it uses dict and only build dataframe at the end 
-        
-        if len(emergence_result) > 1:
-            for key in emergence_result:
-                df_temp = pd.DataFrame({'value': [emergence_result[key]], 'measure': [key],
-                                        **{pname: [p] for pname, p in temp_param_dict.items()}})
-                emergence_df_temp.append(df_temp)
-                
-        else:
-            df_temp = pd.DataFrame(df_temp = pd.DataFrame({'value': [emergence_result], 'measure': [measure_name],
-                                    **{pname: [p] for pname, p in temp_param_dict.items()}}))
-            emergence_df_temp.append(df_temp)
-    return pd.concat(emergence_df_temp, ignore_index = True)
 
 
-def generate_two_node_data(coupling_vec=None, npoints=None, time_lag=None, noise_corr_vec=None):
-    coupling_matrix = np.matrix([[coupling_vec, coupling_vec], [coupling_vec, coupling_vec]])
-    sim_data = ecmc.mvar_sim_data(coupling_matrix, npoints = npoints, time_lag = time_lag, err = noise_corr_vec)
-    return sim_data
+#%%
+
+# ALLOCATE MODEL & MEASURE PARAMETERS IN RESPECTIVE DICTIONARIES 
+
+# model-function pairs where keys give the model and values give the corresponding function for simulation;
+# keys in model_functions and model_variables need to be the same 
+model_functions =       {'2node_mvar': ds.generate_2node_mvar_data}
+
+# model-variables pairs where keys give the model and values give the corresponding parameters of the model
+model_variables =       {'2node_mvar': ['coupling', 'noise_corr', 'time_lag', 'npoints', 'macro_func_mvar', 'micro_func_mvar']}
+
+# measure-function pairs, where keys give the measure, and values the corresponding function;
+# keys in emergence_measures and measure_variables need to be the same
+emergence_measures =    {'causal_emergence_phiid': ecmc.causal_emergence_phiid,
+                         'causal_emergence_practical': ecmc.causal_emergence_practical} 
+
+# variable names need to be the same as parameter names in the measure functions
+measure_variables =     {'causal_emergence_phiid': ['micro', 'redundancy_func', 'time_lag'],
+                         'causal_emergence_practical': ['macro', 'micro', 'time_lag']}
+
+# all parameters relevant for either measures or data generation
+parameters =            {'time_lag': [1, 10, 100], 
+                         'coupling': np.linspace(0.045, 0.45, num = 10),
+                         'noise_corr': np.linspace(0.01, 0.9, num = 10),
+                         'redundancy_func': ['mmi','ccs'],
+                         'npoints': [2000],
+                         'macro_func_mvar': [ds.sum_micro_mvar],
+                         'micro_func_mvar': [ds.raw_micro_mvar]}
+
+# FINAL GOAL:
+# emergence_measures =  {'causal_emergence_phiid': ecmc.causal_emergence_phiid, 
+#                        'causal_emergence_practical': ecmc.causal_emergence_practical, 
+#                        'dynamical_independence': ecmc.dynamical_independence, 
+#                        'g_emergence': ecmc.g_emergence}
+
+# model_functions =     {'2node_mvar': generate_2node_mvar_data,
+#                        '8node_mvar_global_coup': generate_8node_global_coup_data, 
+#                        '8node_mvar_er': generate_8node_mvar_er_data,
+#                        '8node_mvar_diff_top': generate_8node_mvar_diff_top_data}
+
+# load file for different 8-node MVAR architectures
+# file_to_read = open(data_pathin+r'/various_nets.pkl', 'rb') 
+# various_nets = pickle.load(file_to_read) 
+
+# parameters =          {'time_lag': [1, 10, 1000], 
+#                        'coupling': np.linspace(0.045, 0.45, num = 10),
+#                        'noise_corr': np.linspace(0.01, 0.9, num = 10),
+#                        'redundancy_func': ['mmi','ccs'],
+#                        'npoints': [2000, 10000],
+#                        '8node_architectures': various_nets,
+#                        'global_coupling': np.linspace(0.05, 0.9, num = 10),
+#                        'density_vec': np.linspace(0.05, 0.9, num = 10),
+#                        'macro_mvar': [sum_micro_mvar],
+#                        'micro_mvar': [raw_val_mvar],
+#                        'macro_kuramoto': [sigma_chi_kuramoto, pair_sync_kuramoto],
+#                        'micro_kuramoto': [raw_val_kuramoto, phase_kuramoto, sync_kuramoto, sync_bin_kuramoto]}
+
+# model_variables =     {'2node_mvar': ['coupling', 'noise_corr', 'time_lag', 'npoints'], 
+#                        '8node_mvar_er': ['density_vec', 'noise_corr', 'time_lag', 'npoints'],
+#                        '8node_mvar_global_coup': ['coupling', 'noise_corr', 'time_lag', 'npoints'],
+#                        '8node_mvar_diff_top': ['coupling_matrix', 'noise_corr', 'time_lag', 'npoints'],
+#                        '8node_kuramoto': ['coupling', 'beta', 'time_lag', 'npoints'],
+#                        '256node_kuramoto': ['coupling', 'beta', 'time_lag', 'npoints']}
+
+# measure_variables =   {'causal_emergence_phiid': ['micro', 'redundancy_func', 'time_lag'],
+#                        'causal_emergence_practical': ['macro', 'micro',  'time_lag'],
+#                        'dynamical_independence': ['macro', 'micro',  'time_lag'],
+#                        'g_emergence': ['macro', 'micro', 'time_lag']}
+
+#%%
+
+# compute emergence for all parameter combinations
+emergence_df = ecmc.compute_emergence(model_functions, model_variables, emergence_measures, measure_variables, parameters)
+
+
+            
     
-emergence_measures = {
-    "causal_emergence_phiid": ecmc.causal_emergence_phiid,}
-    #"causal_emergence_practical": ecmc.causal_emergence_practical} #, "dynamical_independence", "g_emergence"]
-
-# model dict contains model: data_function pairs, where data_function generates the data for this model
-model_dict = {"2node": generate_two_node_data}
-
-
-parameter_dict = {"time_lag": [1, 3], 
-"coupling_vec": np.linspace(0.045, 0.45, num = 10),
-"noise_corr_vec": np.linspace(0.01, 0.9, num = 10),
-"redundancy_func": ['mmi','ccs'],
-"npoints": [2000]
-}
-
-# TODO: variable names need to be the same as parameter names in the measure functions
-measure_variables = {"causal_emergence_phiid": ["redundancy_func", "time_lag"]}
-
-# needs to be the same name as above in model_dict
-model_variables = {"2node": ["coupling_vec", "noise_corr_vec", "time_lag", "npoints"]}
-
-# ----------------------------------------------------------------------------
-# 2-node networks with differing noise correlation & coupling strength
-# ----------------------------------------------------------------------------
-
-from functools import partial
-
-random.seed(10)
-
-# WAS DONE
-# TODO: in for loops separate parameters for data generation and parameters for different emergence parameters
-# WAS DONE
-
-# TODO: named product generator for dict of iterables
-def named_product(ddict): pass
-
-big_df_list = []
-
-for model in model_dict:
-    for params in product(*[            # params is one parameter combinaton from list generated below
-            parameter_dict[param_name] for param_name in model_variables[model]]):  #this line give a list, where first element are the 100 couplings
-        temp_param_dict = {param_name: param for param_name, param in zip(model_variables[model], params)} # just add variable names to param combination
-        generated_data = model_dict[model](**temp_param_dict) # generate data for this parameter combination
-        for measure in emergence_measures: # now generate results for each measure
-            measure_param_dict = {
-                param_name: parameter_dict[param_name] for param_name in measure_variables[measure] if param_name not in temp_param_dict}
-            data_params_for_measure = {param_name: param for param_name, param in temp_param_dict.items() if param_name in measure_variables[measure]}
-            partial_measure_func = partial(emergence_measures[measure], **data_params_for_measure)
-            temp_df = get_results_for_measure(emergence_measures[measure], measure_param_dict, generated_data, measure)
-            # now add data parameters to dataframe
-            temp_df = temp_df.assign(**temp_param_dict)
-            big_df_list.append(temp_df)
-
-b = pd.concat(big_df_list, ignore_index = True)
-#            # TODO: below should be in a function
-#            for measure_params in product(*measure_param_dict.values()):
-#                temp_param_dict_ms = {param_name: param for param_name, param in zip(measure_param_dict, params)}
-#                # now put together the data params and measure params and let the measure func sort them out
-#                measure_result = emergence_measures[measure](**{**temp_param_dict_ms, **temp_param_dict})
-    
-
-
-# params: (0.045, 0.01, 1, 2000)
-# param: 0.045
-# model_variables[model]: ['coupling_vec', 'noise_corr_vec', 'time_lags', 'npoints']
-# mode: "2node"
-# param_name: 'coupling_vec'
-
-
-    
-    
-
 
 
     
