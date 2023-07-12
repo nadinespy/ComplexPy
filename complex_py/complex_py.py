227f056db8c97848cd531bf50b21adf837f0dc24
nadinespy <nadine.spychala@gmail.com>
2023-07-12 19:10:44 +0100

    changed README

--- complex_py/complex_py.py
+++ complex_py/complex_py.py
@@ -17,19 +17,6 @@ import matlab.engine
 # define Matlab engine
 eng = matlab.engine.start_matlab()
 
-
-#__all__ = ["causal_emergence_phiid", "causal_emergence_practical", "mvar_sim_data", "phiid", "cumgauss"] 
-# "compute_emergence", 
-
-# Use duecredit (duecredit.org) to provide a citation to relevant work to
-# be cited. This does nothing, unless the user has duecredit installed,
-# And calls this with duecredit (as in `python -m duecredit script.py`):
-# due.cite(Doi("10.1167/13.9.30"),
-#          description="Template project for small scientific Python projects",
-#          tags=["reference-implementation"],
-#          path='complex_py')
-
-
 # -----------------------------------------------------------------------------
 # CAUSAL EMERGENCE (PHIID & PRACTICAL)
 # -----------------------------------------------------------------------------
@@ -37,7 +24,7 @@ eng = matlab.engine.start_matlab()
 
 def causal_emergence_phiid(data_dict, time_lag = 1, redundancy_func = 'mmi'):
     """
-    Purpose : ...
+    Purpose : Compute PhiID-based Causal Emergence.
     
     Parameters
     ----------
@@ -54,19 +41,6 @@ def causal_emergence_phiid(data_dict, time_lag = 1, redundancy_func = 'mmi'):
         'causal_emergence_phiid', 'downward_causation_phiid', 
         'causal_decoupling_phiid', and value is float
         Causal emergence, downward causation, causal decoupling based on PhiID.
-        
-    Notes
-    -----
-    ...
-
-    ADJUST TO SPHYNX FROMATTING?
-    Calculate the nth Fibonacci number.
-
-    A recursive implementation of Fibonacci array elements.
-
-    :param n: integer
-    :raises ValueError: raised if n is less than zero
-    :returns: Fibonacci number
     """
     #TODO: change to isinstance
     if not isinstance(data_dict, dict):
@@ -123,7 +97,7 @@ def causal_emergence_phiid(data_dict, time_lag = 1, redundancy_func = 'mmi'):
 
 def causal_emergence_practical(data_dict, time_lag = 1):
     """
-    Purpose : ...
+    Purpose : Compute Shannon-based Causal Emergence.
     
     Parameters
     ----------
@@ -139,10 +113,6 @@ def causal_emergence_practical(data_dict, time_lag = 1):
         'causal_decoupling_pract', and value is float
         Causal emergence, downward causation, causal decoupling based on 
         standard Shannon information.
-    
-    Notes
-    -----
-    ...
 
     """
     
@@ -178,7 +148,7 @@ def causal_emergence_practical(data_dict, time_lag = 1):
     
 def phiid_2sources_2targets(micro, time_lag = 1, redundancy_func = 'mmi'):
     """
-    Purpose : ...
+    Purpose : Compute Integrated Information Decomposition.
     
     Parameters
     ----------
@@ -193,10 +163,6 @@ def phiid_2sources_2targets(micro, time_lag = 1, redundancy_func = 'mmi'):
     -------
     phiid : Matlab struct
         Emergence capacity, downward causation, causal decoupling.
-    
-    Notes
-    -----
-    ...
 
     """
     
@@ -243,6 +209,26 @@ def phiid_2sources_2targets(micro, time_lag = 1, redundancy_func = 'mmi'):
 # -----------------------------------------------------------------------------
 
 def get_result_for_measure(measure_func, measure_params_dict, data_dict, measure_name):                          
+    """
+    Purpose : Compute Integrated Information Decomposition.
+    
+    Parameters
+    ----------
+    measure_func : function
+        Function to calculate a measure.
+    measure_params_dict : dictionary
+        Dictionary with measure parameters.
+    data_dict : dictionary
+        Dictionary with model parameters.
+    measure_name : string
+	Measure.
+    Returns
+    -------
+    emergence_df_temp : dataframe
+        Includes measure, measure and model parameters.
+
+    """
+    
     emergence_df_temp = []
     
     if type(data_dict) != dict:
@@ -266,6 +252,29 @@ def get_result_for_measure(measure_func, measure_params_dict, data_dict, measure
     return pd.concat(emergence_df_temp, ignore_index = True)
 
 def compute_emergence(model_functions, model_variables, emergence_functions, measure_variables, parameters):
+    """
+    Purpose : Compute all measures for all measure and model parameters.
+    
+    Parameters
+    ----------
+    model_functions : dictionary
+        Dictionary with function names and functions.
+    model_variables : dictionary
+        Dictionary with model variables.
+    emergence_functions : dictionary
+        Dictionary with function names and functions.
+    measure_variables : dictionary
+	Dictionary with measure variables.
+    parameters : dictionary
+        All measure and model parameters.
+        
+    Returns
+    -------
+    emergence_df : dataframe
+        Includes all measures, and measure and model parameters.
+
+    """
+    
     emergence_df_temp = []
     
     if type(model_functions) != dict:
@@ -299,7 +308,6 @@ def compute_emergence(model_functions, model_variables, emergence_functions, mea
     # TODO: assert that each value in model_functions and emergence_functions is a function
         
   
-
     for model in model_functions:
         for params in product(*[parameters[param_name] for param_name in model_variables[model]]):          
             model_params_dict = {param_name: param for param_name, param in 
@@ -354,145 +362,3 @@ def compute_emergence(model_functions, model_variables, emergence_functions, mea
 
 
 
-
-
-# UNDERSTANDING THE ABOVE LOOPS - EXAMPLE
-# 
-# GIVEN VARIABLES:
-# emergence_functions =  {'causal_emergence_phiid': ecmc.causal_emergence_phiid}
-# model_functions =     {'2node_mvar': generate_2node_mvar_data}
-
-# parameters =          {'time_lag': [1, 3], 
-#                        'coupling': np.linspace(0.045, 0.45, num = 10),
-#                       'noise_corr': np.linspace(0.01, 0.9, num = 10),
-#                       'redundancy_func': ['mmi','ccs'],
-#                       'npoints': [2000]}
-# measure_variables =   {'causal_emergence_phiid': ['redundancy_func', 'time_lag']}
-# model_variables =     {'2node_mvar': ['coupling', 'noise_corr', 'time_lag', 'npoints']}
-
-# WHAT THE VARIABLES IN THE LOOPS MEAN:
-# params:                       can be (0.045, 0.01, 1, 2000)
-# param:                        can be 0.045
-# model_variables[model]:       ['coupling', 'noise_corr', 'time_lags', 'npoints']
-# mode:                         '2node_mvar'
-# param_name:                   can be 'coupling'
-# model:                        '2node_mvar'
-# model_variables[model]:       ['coupling', 'noise_corr', 'time_lag', 'npoints']
-# measure:                      'causal_emergence_phiid'
-# emergence_functions[measure]:  ecmc.causal_emergence_phiid
-# measure_variables[measure]:   ['redundancy_func', 'time_lag']
-# measure_param_dict:           can be {'micro': ds.raw_micro_mvar, 'time_lag': [3], 'redundancy_func': ['ccs']}                                
-#                               --> includes parameters relevant for measure
-# model_params_dict:            can be {'coupling': 0.045, 'noise_corr': 0.01, 'time_lag': 1, 'npoints': 2000}      
-#                               --> includes parameters relevant for data generation
-# data_params_for_measure:      can be {'time_lag': [1]}                                                            
-#                               --> includes parameters relevant to both measure and data generation
-# partial_measure_func:         is a function which will take parameters shared between measure and 
-#                               data generation as separate inputs
-# df_temp:                      can be {'value': [0.0332], 'measure': ['phiid_downward_causation'], 'time_lag': [1], 
-#                               'redundancy_func': ['ccs']} *before* adding data parameters, and
-#                               {'value': [0.0332], 'measure': ['phiid_downward_causation'], 'time_lag': [1], 
-#                               'redundancy_func': ['ccs'], 'coupling': [0.045], 'noise_corr': [0.01], 
-#                               'time_lag': [1], 'npoints': [2000]} *after* adding data parameters
-
-
-# GOING THROUGH THE LOOPS:
-# for [one model, like'2node_mvar'] in [variable storing all models and corresponding data generation function, 
-#      in this case model_functions]:
-    
-#     for [one parameter combination, like (0.045, 0.01, 1, 2000)] in product([all possible parameter combinations]):
-#         model_params_dict = [store parameter names and parameter values in dict, 
-#                              like {'coupling': 0.045, 'noise_corr': 0.01, 'time_lag': 1, 'npoints': 2000}]
-#         data_dict = generate_2node_mvar_data(**model_params_dict) 
-#                     --> generate_2node_mvar_data() will use keys as variable names and assign it the respective values
-
-#         for [one measure, like 'causal_emergence_phiid'] in [variable which stores all measures and corresponding 
-#              function to calculate it, in this case emergence_functions]:
-#             measure_param_dict = [store parameter names and parameter values in dict - but only those which are 
-#                                   not already in model_params_dict! -, like {'redundancy_func': ['ccs']}]
-#             data_params_for_measure = [store parameter names and parameter values in dict - but only those which 
-#                                       are in *both* model_params_dict and measure_param_dict, like {'time_lag': [1]}]
-#             partial_measure_func = partial(emergence_functions[measure], **data_params_for_measure)
-#             df_temp = get_result_for_measure(emergence_functions[measure], measure_param_dict, data_dict, measure)
-#             df_temp = df_temp.assign(**model_params_dict)
-#             big_df_list.append(df_temp)
-
-# comprehensive lists:
-# for params in product(*[parameters[param_name] for param_name in model_variables[model]]): for one set of parameters, 
-#                         take value of key of each key in model_variables of the respective model
-# general way comprehensive lists work: do X for Y in Z
-# product = product(*[parameters[param_name] for param_name in model_variables['2node_mvar']]) 
-# what is the output of product()?
-
-# BOILING DOWN THE LOOPS ONCE MORE:
-# for generate_2node_mvar_data:
-#     for (0.045, 0.01, 1, 2000): 
-#         # we need to go from tuple of parameters to dictionary with parameter names as keys and parameters as values 
-#         # to use named arguments for functions 
-#         model_params_dict = {'coupling': 0.045, 'noise_corr': 0.01, 'time_lag': 1, 'npoints': 2000}
-#         data_dict = generate_2node_mvar_data(coupling = 0.045, npoints = 2000, time_lag = 1, noise_corr = 0.01)
-    
-#         for ecmc.causal_emergence_phiid:
-#             # we need to go from tuple of parameters to dictionary with parameter names as keys and parameters as values 
-#             # to use named arguments for functions 
-#             measure_param_dict = {'time_lag': [3], 'redundancy_func': ['ccs']}
-#             df_temp = get_result_for_measure(ecmc.causal_emergence_phiid, measure_param_dict, data_dict, 
-#                                              'causal_emergence_phiid')
-#             # get_result_for_measure() will be equal to {'value': [0.0332], 'measure': ['phiid_downward_causation'], 
-#                                                          'time_lag': [1], 'redundancy_func': ['ccs']} 
-#                                                           --> includes only measure parameters!
-#             df_temp = df_temp.assign(**model_params_dict)
-#             # df_temp will be equal to {'value': [0.0332], 'measure': ['phiid_downward_causation'], 'time_lag': [1], 
-#                                         'redundancy_func': ['ccs'], 'coupling': [0.045], 'noise_corr': [0.01], 
-#                                         'time_lag': [1], 'npoints': [2000]} 
-#                                          --> includes *both* measure and data parameters!
-#             big_df_list.append(df_temp)
-
-
-
-
-# UNDERSTANDING get_result_for_measures(measure_func, measure_params_dict, data, measure_name):
-# function is called as: df_temp = get_result_for_measure(emergence_functions[measure], measure_param_dict, 
-#                                                         data_dict, measure) 
-#
-# INPUT VARIABLES:
-# emergence_functions[measure] = cp.causal_emergence_phiid 
-# measure_param_dict =          {'time_lag': [3], 'redundancy_func': ['ccs']}
-# measure =                     'causal_emergence_phiid'
-#                               data_dict
-
-# WHAT THE VARIABLES IN THE LOOPS MEAN:
-# params:               can be (0.045, 0.01, 1, 2000)  
-# param:                can be 0.045 
-# param_name:           can be 'coupling'
-# model_params_dict:    can be {'time_lag': [3], 'redundancy_func': ['ccs']}
-# df_temp:              can be {'value': [0.0332], 'measure': ['phiid_downward_causation'], 'time_lag': [1], 
-#                               'redundancy_func': ['ccs']}
-# emergence_result:     can be {'phiid_emergence_capacity': [0.0332], 'phiid_downward_causation': [0.0456], 
-#                               'phiid_causal_decoupling': [0.0222]}
-
-# GOING THROUGH THE LOOPS:
-# for  [one parameter combination, like {3,'ccs'}] in product([all possible parameter combinations]): 
-    # model_params_dict = [store parameter names and parameter values in dict, 
-    #                      like {'time_lag': [3], 'redundancy_func': ['ccs']}]  
-    # emergence_result = cp.causal_emergence_phiid(data_dict, **model_params_dict)
-    # for key in emergence_result:
-    #     df_temp = pd.DataFrame({'value': [emergence_result[key]], 'measure': [key], 
-    #                              **{pname: [p] for pname, p in model_params_dict.items()}})
-    #     emergence_df_temp.append(df_temp)
-    
-    # pd.concat(emergence_df_temp, ignore_index = True)
-
-# BOILING DOWN THE LOOPS ONCE MORE:
-# for {3,'ccs'}: 
-#     model_params_dict = {'time_lag': [3], 'redundancy_func': ['ccs']}
-#     emergence_result = 0.0465
-#     for 'phiid_downward_causation':
-#         df_temp = pd.DataFrame({'value': [0.0332], 'measure': ['phiid_downward_causation'], 
-#                                 'time_lag': [1], 'redundancy_func': ['ccs']})
-
-# comprehensive lists:
-# for params in product(*[params for pname, params in measure_param_dict.items()]): 
-# for one set of parameters, get values of keys for key and value of key in measure_param_dict
-
- 
